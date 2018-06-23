ec36d53f4f7aad06f872ce76f285a9da1943aa9f
pedropmedina <pedrodelilia@gmail.com>
2018-06-23 18:43:28 -0400

    Complete test for CommetBox component

--- src/tests/components/CommentBox.test.js
+++ src/tests/components/CommentBox.test.js
@@ -17,3 +17,22 @@ test('should contain a textarea and button', () => {
 	expect(wrapper.find('textarea').length).toEqual(1);
 	expect(wrapper.find('button').length).toEqual(1);
 });
+
+describe('the textarea', () => {
+	beforeEach(() => {
+		wrapper.find('textarea').simulate('change', {
+			target: { value: 'new comment' },
+		});
+		wrapper.update();
+	});
+
+	test('should track changes in text area', () => {
+		expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
+	});
+
+	test('should empty textarea value on form submission', () => {
+		wrapper.find('form').simulate('submit');
+		wrapper.update();
+		expect(wrapper.find('textarea').prop('value')).toEqual('');
+	});
+});
