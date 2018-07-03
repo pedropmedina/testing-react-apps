cb97c5a767e2e2da7e524fc428e5555e2e7aadd5
pedropmedina <pedrodelilia@gmail.com>
2018-07-02 23:31:59 -0400

    Added middleware examples

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+// import async from '../middlewares/async';
+import stateValidator from '../middlewares/stateValidator';
+
 import reducers from '../reducers/index';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -9,7 +12,7 @@ export default initialState => {
 	const store = createStore(
 		reducers,
 		initialState,
-		composeEnhancers(applyMiddleware(thunk)),
+		composeEnhancers(applyMiddleware(thunk, stateValidator)),
 	);
 	return store;
 };
